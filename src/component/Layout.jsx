0c6319b18025c705ad220f08986e3275a5feaf8b
Zheheng <henryyzh0309@gmail.com>
2025-10-27 12:19:33 +0800

    Layout creation

---
+++ src/component/Layout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import "../index.css";
+
+
+const Layout = ({ children }) => {
+    return (
+        <div className="layout">
+            <Sidebar />
+
+            <div className="main-content">
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Layout
